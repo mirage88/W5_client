b4d4d98a8a45003b197b0a940f127badcdab0165
Joel Leibow <jleibow@gmail.com>
2012-11-10 20:16:54 -0700

    Got the projects button to create a modal via twitter bootstrap.

--- js/app/views/projects/list_view.js
+++ js/app/views/projects/list_view.js
@@ -1,15 +1,20 @@
 define([
     'vendor/backbone',
     'vendor/underscore',
+    'app/routers/app/app_router',
     'text!templates/projects/list.html',
     'vendor/bootstrap'
-  ], function(Backbone, _, projectsListTemplate) {
+  ], function(Backbone, _, appRouter, projectsListTemplate) {
     var listView = Backbone.View.extend({
       el: $('#projects'),
       render: function() {
         this.$el.addClass('modal');
         this.$el.append(_.template(projectsListTemplate));
         this.$el.modal();
+        this.$el.on('hidden', { elObj: this }, function(ev) {
+          ev.data.elObj.$el.empty();
+          appRouter.initialize();
+        });
       }
     });
 
